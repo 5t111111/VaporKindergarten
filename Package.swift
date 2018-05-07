9ed4c7ffbccf1e442fc94385c04c71e0f0eeca94
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-07 14:16:02 +0900

    Update vapor version to 3.0.0

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ let package = Package(
     name: "VaporKindergarten",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🍃 Leaf template engine.
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2"),
 
         // 🖋🐬 Swift ORM (queries, models, relations, etc) built on MySQL.
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2.4"),
 
         // Crypto
         .package(url: "https://github.com/vapor/crypto.git", from: "3.1.1")
